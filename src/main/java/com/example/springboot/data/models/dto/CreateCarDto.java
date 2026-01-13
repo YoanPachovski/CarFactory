7bf44ed9bbccad33596e70b66e83555ea9386d42
Yoan Pachovski <ba4ikoki1999@gmail.com>
2026-01-13 17:16:24 +0200

    decreased brand leftover

---
+++ src/main/java/com/example/springboot/data/models/dto/CreateCarDto.java
@@ -0,0 +1,11 @@
+package com.example.springboot.data.models.dto;
+
+import com.example.springboot.data.models.entities.BaseEntity;
+
+
+public class CreateCarDto {
+    private String model;
+    private String registrationNumber;
+    private double price;
+    private String brand;
+}
---
+++ src/main/java/com/example/springboot/service/CarService.java
@@ -0,0 +1,54 @@
+package com.example.springboot.service;
+
+import com.example.springboot.data.models.dto.CreateCarDto;
+import com.example.springboot.data.models.dto.CarDto;
+import com.example.springboot.data.models.entities.Car;
+import com.example.springboot.data.repo.CarRepo;
+import com.example.springboot.util.MapperUtil;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarService {
+    MapperUtil mapperUtil;
+    CarRepo CarRepo;
+    CarDto carDto;
+    public CreateCarDto createCarDto(Car Car) {
+        return mapperUtil.getModelMapper()
+                .map(this.CarRepo
+                        .save(mapperUtil.getModelMapper()
+                                .map(Car, Car.class)), CreateCarDto.class);
+
+    }
+
+    public CarDto getCar(long id) {
+        return this.mapperUtil.getModelMapper()
+                .map(this.CarRepo.findById(id),
+                        CarDto.class);
+    }
+    public List<CarDto> getAllCars() {
+        return this.mapperUtil
+                .mapList(
+                        this.CarRepo.findAll(), CarDto.class);
+    }
+
+    public Car updateCar(Car Car, long id) {
+//        Car == json Entity
+//        Car1 == db Entity
+//        if Car1.field == null, then Car.field = Car.field
+        return this.CarRepo.findById(id)
+                .map(Car1 -> {
+                    Car1.setBrand(Car.getBrand() == null ? Car1.getBrand() : Car.getBrand());
+                    Car1.setPrice(Car.getPrice() == 0.0 ? Car1.getPrice() : Car.getPrice());
+                    Car1.setModel(Car.getModel() == null ? Car1.getModel() : Car.getModel());
+                    Car1.setRegistrationNumber(Car.getRegistrationNumber() == null ? Car1.getRegistrationNumber() : Car.getRegistrationNumber());
+                    return this.CarRepo.save(Car1);
+                })                .orElseThrow(() -> new RuntimeException("Car with id=" + id + " not found!"));
+    }
+
+    public void deleteCar(long id) {
+        this.CarRepo.deleteById(id);
+    }
+
+}
